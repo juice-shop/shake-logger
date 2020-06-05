809ef6c7073eeccde81cf836227985b29ed0350a
Timo Pagel <github@timo-pagel.de>
2020-06-05 06:24:34 +0200

    fix logger env

--- logger.php
+++ logger.php
@@ -3,7 +3,7 @@
 $logger = "/tmp/logger.php";
 
 if ($_REQUEST['input']) {
-        file_put_contents($logger, 'IP: ' . $_SERVER['REMOTE_ADDR'] . 'Date: ' . date('Y-m-d H:i:s', time()) . ", Text: " . $_REQUEST['input']. "\n", FILE_APPEND);
+        file_put_contents($logger, 'IP: ' . $_SERVER['REMOTE_ADDR'] . ', Date: ' . date('Y-m-d H:i:s', time()) . ", Text: " . $_REQUEST['input']. "\n", FILE_APPEND);
 } else {
 ?><html>
 <head>

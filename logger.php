3fd74d78b591f690c69afd44e66090125d33a88a
Timo Pagel <github@timo-pagel.de>
2020-07-31 08:57:41 +0200

    Remove }

--- logger.php
+++ logger.php
@@ -4,7 +4,7 @@ $logger = "/tmp/logger.txt";
 
 if (array_key_exists("input", $_REQUEST)) {
         file_put_contents($logger, 'IP: ' . $_SERVER['REMOTE_ADDR'] . ', Date: ' . date('Y-m-d H:i:s', time()) . ", Text: " . $_REQUEST['input']. "\n", FILE_APPEND);
-} else {
+}
 ?><html>
 <head>
     <meta http-equiv="refresh" content="5"/>
